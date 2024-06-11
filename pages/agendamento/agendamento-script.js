4f2c065c89fa818d1ff5df3ce6f3c1bdd1a13739
GabrielPresense <gabrielpresense@gmail.com>
2024-06-11 20:09:34 -0300

    Agendamento

---
+++ pages/agendamento/agendamento-script.js
@@ -0,0 +1,17 @@
+document.getElementById('appointment-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var date = document.getElementById('date').value;
+    var time = document.getElementById('time').value;
+
+    var message = 'Agendamento feito.%0A%0A';
+    message += 'Nome: ' + encodeURIComponent(name) + '%0A';
+    message += 'Telefone: ' + encodeURIComponent(phone) + '%0A';
+    message += 'Data: ' + encodeURIComponent(date) + '%0A';
+    message += 'Hora: ' + encodeURIComponent(time) + '%0A';
+
+    var whatsappLink = 'https://api.whatsapp.com/send?phone=' + encodeURIComponent(phone) + '&text=' + message;
+    window.open(whatsappLink, '_blank');
+});
